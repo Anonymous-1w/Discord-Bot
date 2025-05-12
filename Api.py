0122319728c0cb4725e623f9537a49122d5cf8ef
Anonymous-1w <95274094+Anonymous-1w@users.noreply.github.com>
2025-05-12 18:29:16 +0500

    Update Api.py

--- Api.py
+++ Api.py
@@ -1,15 +1,1 @@
-import requests
 
-url = "https://courses.altacademy.org/admin/api/v2/users/walid.ahmed@altacademy.org"
-
-querystring = {"include_suspended":"true"}
-
-headers = {
-    "Authorization": "Bearer HHvkFXClmYF49GV4dInM1OVHMGnnGqdqhaI5Msal",
-    "Lw-Client": "645a0bc1ebb978f660035e95",
-    "Accept": "application/json"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-
-print(response.json())
